4761fac989f451cae4893beb5b71ec9c95156779
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-06-03 11:14:35 +0100

    added the first four attributes to characters

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,5 @@
+from argparse import Action
+from ctypes.wintypes import HPALETTE
 from flask import render_template, request, redirect
 from app import app
 from models.characters import *
@@ -10,6 +12,18 @@ def index():
 @app.route('/characters', methods=['POST'])
 def new_character():
     name = request.form['name']
-    new_character = Character(name=name)
+    HP = request.form['HP']
+    AC = request.form['AC']
+    new_character = Character(name=name, HP=HP, AC=AC)
     add_new_character(new_character)
+    return redirect('/characters')
+
+@app.route('/characters/remove_HP/<name>', methods=['Post'])
+def lower_hp(name):
+    remove_1_hp(name)
+    return redirect('/characters')
+
+@app.route('/characters/add_HP/<name>', methods=['Post'])
+def raise_hp(name):
+    add_1_hp(name)
     return redirect('/characters')
--- models/character.py
+++ models/character.py
@@ -1,3 +1,5 @@
 class Character():
-    def __init__(self, name):
+    def __init__(self, name, HP, AC):
         self.name = name
+        self.HP = HP
+        self.AC = AC
