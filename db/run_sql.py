0af2b35656e39ce3230c29806775995f1bd8c2c2
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-11-07 11:10:56 +0000

    added default values to form entries and link to lobby

--- db/run_sql.py
+++ db/run_sql.py
@@ -6,7 +6,7 @@ def run_sql(sql, values = None):
     results = []
 
     try:
-        conn=psycopg2.connect("dbname='dnd_web'")
+        conn=psycopg2.connect("dbname='dnd_web_live'")
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
